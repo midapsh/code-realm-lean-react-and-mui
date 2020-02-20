90e2a9ff328ce7dd08246c26479645914c824187
midapsh <34818053+midapsh@users.noreply.github.com>
2020-02-20 11:45:36 -0300

    Change App.js to a component folder

---
+++ 01-intro/src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <>
+      <Button variant="contained">Hello World</Button>
+    </>
+  );
+}
+
+export default App;
