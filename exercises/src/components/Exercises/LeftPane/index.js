d9118ef7e169e59ad0bcbd4d6afe1fca1d390cc7
midapsh <34818053+midapsh@users.noreply.github.com>
2020-02-20 16:51:09 -0300

    Populated left panel

--- exercises/src/components/Exercises/LeftPane/index.js
+++ exercises/src/components/Exercises/LeftPane/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
-import { Paper } from "@material-ui/core";
+import {
+    Paper, Typography,
+    List, ListItem, ListItemText
+} from "@material-ui/core";
 
-export default function LeftPane({ style }) {
+export default function LeftPane({ style, exercisesByMuscles }) {
     return (
         <Paper style={style.Paper}>
-            Left Pane
+            {exercisesByMuscles.map(([group, exercises]) =>
+                <Typography
+                    key={group}
+                    variant="subtitle1"
+                    style={{ textTransform: "capitalize" }}
+                >
+                    {group}
+                </Typography>
+            )}
         </Paper>
     );
 }
+
+// {/* <List component="nav" aria-label="secondary mailbox folders">
+//         <ListItem button>
+//           <ListItemText primary="Trash" />
+//         </ListItem>
+//         <ListItemLink href="#simple-list">
+//           <ListItemText primary="Spam" />
+//         </ListItemLink>
+//       </List> */}
