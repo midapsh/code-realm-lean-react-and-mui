7c037c5df89d9b2eba827ddaf77aa361acc2dd42
midapsh <34818053+midapsh@users.noreply.github.com>
2020-02-20 16:52:27 -0300

    Make Dashboard more dynamic

--- exercises/src/pages/Dashboard/index.js
+++ exercises/src/pages/Dashboard/index.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 import { Header, Footer } from "../../components/Layouts";
 import Exercises from "../../components/Exercises";
 
+import {
+    muscles, exercises
+} from "../../services/store";
+
 export default function Dashboard() {
+    const [exercisesByMuscles, setExercisesByMuscles] = useState([]);
+
+    useEffect(() => {
+        async function loadExercisesByMuscles() {
+            // setExercisesByMuscles(Object.entries(groupBy(exercises)));
+            setExercisesByMuscles(Object.entries(
+                exercises.reduce((exercises, exercise) => {
+                    const { muscles } = exercise
+
+                    exercises[muscles] = exercises[muscles]
+                        ? [...exercises[muscles], exercise]
+                        : [exercise];
+
+                    return exercises;
+                }, {})
+            ));
+        }
+        loadExercisesByMuscles();
+    }, []);
+
     return (
         <>
             <Header />
-            <Exercises />
-            <Footer />
+            <Exercises
+                exercisesByMuscles={exercisesByMuscles}
+            />
+            <Footer
+                muscles={muscles}
+            // onSelect={handleCategorySelected}
+            />
         </>
     );
 }
