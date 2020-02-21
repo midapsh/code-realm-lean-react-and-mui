c8dc5859d1cbf8648a05bd299155bfe90542be68
midapsh <34818053+midapsh@users.noreply.github.com>
2020-02-21 19:07:46 -0300

    Removing weird spaces

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import Routes from "./routes";
 
 function App() {
   return (
     <>
+      <CssBaseline />
       <Routes />
     </>
   );
