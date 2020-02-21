d012b2b80b75ed3904f6d00897b70bdda50f8d13
midapsh <34818053+midapsh@users.noreply.github.com>
2020-02-21 15:38:52 -0300

    Created form insert

--- exercises/src/components/Dialog/Create/index.js
+++ exercises/src/components/Dialog/Create/index.js
@@ -1,19 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import {
   Button,
   Fab,
+  TextField,
+} from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+
+import {
   Dialog,
   DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle,
+} from "@material-ui/core";
+
+import {
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
 } from '@material-ui/core';
-import AddIcon from "@material-ui/icons/Add";
 
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function Create() {
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(1),
+    // minWidth: 500,
+    width: 500,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+export default function Create({ musclesCategories, onCreate }) {
+  const classes = useStyles();
   const [open, setOpen] = useState(false);
+  const [exercise, setExercise] = useState({ title: "", description: "", muscles: "" });
+
 
   async function handleClickOpen() {
     setOpen(true);
@@ -23,23 +49,80 @@ export default function Create() {
     setOpen(false);
   }
 
+  async function handleChange(name, value) {
+    setExercise({ ...exercise, [name]: value })
+  }
+
+  async function handleSubmit(event) {
+    // TODO: Validate
+    event.preventDefault();
+    onCreate({
+      ...exercise,
+      id: exercise.title.toLocaleLowerCase().replace(/ /g, "-"),
+    });
+    setExercise({ title: "", description: "", muscles: "" });
+    setOpen(false);
+  }
   return (
     <>
       <Fab size="small" aria-label="add" onClick={handleClickOpen}>
         <AddIcon />
       </Fab>
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Create new Exercise</DialogTitle>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle id="form-dialog-title">
+          Create new Exercise
+        </DialogTitle>
         <DialogContent>
           <DialogContentText>
             Please fill out the form below
           </DialogContentText>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              id="title"
+              label="title"
+              className={classes.formControl}
+              value={exercise.title}
+              onChange={event => handleChange("title", event.target.value)}
+              autoFocus
+              margin="normal"
+            />
+            <br />
+            <FormControl className={classes.formControl}>
+              <InputLabel id="muscles">Muscles</InputLabel>
+              <Select
+                labelId="muscles-label"
+                id="muscles-select"
+                value={exercise.muscles}
+                onChange={event => handleChange("muscles", event.target.value)}
+              >
+                {musclesCategories.map(category =>
+                  <MenuItem key={category} value={category}>{category}</MenuItem>
+                )}
+              </Select>
+            </FormControl>
+            <br />
+            <TextField
+              id="description"
+              label="description"
+              className={classes.formControl}
+              value={exercise.description}
+              onChange={event => handleChange("description", event.target.value)}
+              autoFocus
+              margin="normal"
+              multiline
+              rows="4"
+            />
+          </form>
         </DialogContent>
-        <form>
-
-        </form>
         <DialogActions>
-          <Button color="primary" variant="contained">
+          <Button color="primary"
+            variant="contained"
+            type="submit"
+            onClick={handleSubmit}
+          >
             Create
           </Button>
         </DialogActions>
--- exercises/src/components/Layouts/Header.js
+++ exercises/src/components/Layouts/Header.js
@@ -6,15 +6,18 @@ import {
 
 import CreateDialog from "../Dialog/Create";
 
-export default function Header() {
+export default function Header({ muscles, onExerciseCreate }) {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" color="inherit" style={{flex: 1}}>
+        <Typography variant="h6" color="inherit" style={{ flex: 1 }}>
           My Exercise DataBase
         </Typography>
 
-        <CreateDialog />
+        <CreateDialog
+          musclesCategories={muscles}
+          onCreate={onExerciseCreate}
+        />
       </Toolbar>
     </AppBar>
   );
